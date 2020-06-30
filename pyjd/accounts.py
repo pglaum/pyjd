1fec2ed32148c0ad5a3f220242a0855027080461
pglaum <p@pglaum.de>
2020-06-30 14:05:46 +0200

    document accounts

--- pyjd/accounts.py
+++ pyjd/accounts.py
@@ -1,16 +1,19 @@
 from .jd import make_request
 from .jd_types import Account, AccountQuery, BasicAuth, BasicAuthType
+from typing import Dict, List
 
 endpoint = 'accountsV2'
 
 
-def action(route, params=None):
+def action(route: str, params: any=None) -> any:
     route = f'{endpoint}{route}'
     return make_request(route, params)
 
 
-def add_account(premium_hoster, username, password):
+def add_account(premium_hoster: str, username: str, password: str) -> None:
     """Add a premium hoster account.
+
+    .. warning:: The password is used in plain text, so beware...
 
     :param premium_hoster: The name of the premium hoster.
         See ``list_premium_hoster`` for possible names.
@@ -20,15 +23,15 @@ def add_account(premium_hoster, username, password):
     :param password: Password for that account.
     :type password: str
     :return: empty
-    :rtype: str
+    :rtype: None
     """
 
     params = [premium_hoster, username, password]
-    resp = action('/addAccount', params)
-    return resp
+    action('/addAccount', params)
 
 
-def add_basic_auth(auth_type, hostmask, username, password):
+def add_basic_auth(auth_type: BasicAuthType, hostmask: str, username: str,
+        password: str) -> int:
     """Add a basic auth account.
 
     These accounts are plain HTTP or FTP connections, with passwords.
@@ -53,35 +56,31 @@ def add_basic_auth(auth_type, hostmask, username, password):
     return resp
 
 
-def disable_accounts(account_ids):
+def disable_accounts(account_ids: List[int]) -> None:
     """Disable premium hoster accounts.
 
     :param account_ids: A list of account uuids.
     :type account_ids: List[int]
     :return: empty
-    :rtype: str
+    :rtype: None
     """
 
     params = [account_ids]
-    resp = action('/disableAccounts', params)
-    return resp
+    action('/disableAccounts', params)
 
 
-def enable_accounts(account_ids):
+def enable_accounts(account_ids: List[int]) -> None:
     """Enable premium hoster accounts.
 
     :param account_ids: A list of account uuids.
     :type account_ids: List[int]
-    :return: empty
-    :rtype: str
     """
 
     params = [account_ids]
-    resp = action('/enableAccounts', params)
-    return resp
+    action('/enableAccounts', params)
 
 
-def get_premium_hoster_url(hoster):
+def get_premium_hoster_url(hoster: str) -> str:
     """Get the url for a premium hoster.
 
     Note: The url will be a redirect over the jdownloader.org servers.
@@ -97,7 +96,8 @@ def get_premium_hoster_url(hoster):
     return resp
 
 
-def list_accounts(account_query=AccountQuery()):
+def list_accounts(
+        account_query: AccountQuery = AccountQuery()) -> List[Account]:
     """List premium hoster accounts.
 
     :param params: An AccountQuery object.
@@ -117,7 +117,7 @@ def list_accounts(account_query=AccountQuery()):
     return accounts
 
 
-def list_basic_auth():
+def list_basic_auth() -> List[BasicAuth]:
     """List basic auth accounts.
 
     :return: A list of basic auth accounts.
@@ -134,7 +134,7 @@ def list_basic_auth():
     return basic_auths
 
 
-def list_premium_hoster():
+def list_premium_hoster() -> List[str]:
     """List known premium hosters.
 
     :return: A list of all known premium hosters.
@@ -145,46 +145,44 @@ def list_premium_hoster():
     return resp
 
 
-def list_premium_hoster_urls():
+def list_premium_hoster_urls() -> Dict[str, str]:
     """List known premium hosters with urls.
 
     :return: A map of all known premium hosters to urls.
-    :rtype: Map[str, str]
+    :rtype: Dict[str, str]
     """
 
     resp = action('/listPremiumHosterUrls')
     return resp
 
 
-def refresh_accounts(account_ids):
+def refresh_accounts(account_ids: List[int]) -> None:
     """Let JDownloader refresh the account status for ``account_ids``.
 
     :param account_ids: A list of account ids
     :type account_ids: List[int]
     :return: empty
-    :rtype: str
+    :rtype: None
     """
 
     params = [account_ids]
-    resp = action('/refreshAccounts', params)
-    return resp
+    action('/refreshAccounts', params)
 
 
-def remove_accounts(account_ids):
+def remove_accounts(account_ids: List[int]) -> None:
     """Remove the accounts for ``account_ids``.
 
     :param account_ids: A list of account ids
     :type account_ids: List[int]
     :return: empty
-    :rtype: str
+    :rtype: None
     """
 
     params = [account_ids]
-    resp = action('/removeAccounts', params)
-    return resp
+    action('/removeAccounts', params)
 
 
-def remove_basic_auths(basic_auth_ids):
+def remove_basic_auths(basic_auth_ids: List[int]) -> bool:
     """Remove basic auths for ``basic_auth_ids``.
 
     :param basic_auth_ids: A list of basic auth ids.
@@ -198,7 +196,8 @@ def remove_basic_auths(basic_auth_ids):
     return resp
 
 
-def set_username_and_password(account_id, username, password):
+def set_username_and_password(account_id: int, username: str,
+        password: str) -> bool:
     """Set a new username and password for a premium hoster account.
 
     :param account_id: The ID of the account.
@@ -216,7 +215,7 @@ def set_username_and_password(account_id, username, password):
     return resp
 
 
-def update_basic_auth(basic_authentication):
+def update_basic_auth(basic_authentication: BasicAuth) -> bool:
     """Update the credentials for a basic auth.
 
     .. note ::
