ff357ee8f784982193cdd3ae3826fea142d56f77
Philipp Glaum <p@pglaum.de>
2024-11-22 08:40:57 +0100

    fix version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="pyjd",
-    version="1.0.12",
+    version="1.0.13",
     author="Philipp Glaum",
     author_email="p@pglaum.de",
     description=("A wrapper for the JDownloader API"),
